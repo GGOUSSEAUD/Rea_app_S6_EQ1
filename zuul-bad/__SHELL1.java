2b889fe2f9a7f02ccc3e17d10a6e0f7de943b9e7
Gaëtan GOUSSEAUD <gousseaud.gaetan.pro@gmail.com>
2017-02-08 10:11:48 +0100

    Ajout des fichiers d'origine.

---
+++ zuul-bad/__SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/home/gaetan/T\u00e9l\u00e9chargements/zuul-bad");
+final Game game1 = (Game)__bluej_runtime_scope.get("game1");
+
+
+game1.play();
+
+}}
